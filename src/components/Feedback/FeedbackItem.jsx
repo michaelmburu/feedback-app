3dd63d4ab6c241182597ad073f24f9b36490dee1
Michael Mburu <ichangaimichael@yahoo.com>
2022-08-09 10:27:42 +0300

    - Added FaTimes circle delete button

--- src/components/Feedback/FeedbackItem.jsx
+++ src/components/Feedback/FeedbackItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Card from '../shared/Card/Card'
 import PropTypes from 'prop-types'
-import {FaTimes} from 'react-icons/fa'
+import {FaTimesCircle} from 'react-icons/fa'
 const FeedbackItem = ({item, handleDelete}) => {
 
     return (
             <Card>
                 <div className='num-display'>{item.rating}</div>
                 <button onClick={() => handleDelete(item.id)} className='close'>
-                    <FaTimes color='purple' />
+                    <FaTimesCircle color='purple' />
                 </button>
                 <div className='text-display'>{item.text}</div>
             </Card>
